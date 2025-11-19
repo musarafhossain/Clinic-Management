d3051efcd9f4dcc6bbc621fd08ea0ff554e8e4fd
Musaraf Hossain <146651402+musarafhossain@users.noreply.github.com>
2025-11-19 19:36:20 +0530

    Update client build branch

---
+++ update-client-build.bat
@@ -0,0 +1,28 @@
+@echo off
+echo ===== Updating CLIENT BUILD branch =====
+
+git checkout main
+git pull
+
+git checkout client-build
+
+:: Clean folder but keep .git and all .bat files
+for /f "delims=" %%i in ('dir /a /b') do (
+    if /i not "%%i"==".git" (
+        if /i not "%%~xi"==".bat" (
+            rmdir /s /q "%%i" 2>nul
+            del /f /q "%%i" 2>nul
+        )
+    )
+)
+
+git checkout main -- client/out
+
+:: Move all client files and folders to root
+robocopy client/out . /E /MOVE >nul
+
+git add .
+git commit -m "Update client build branch"
+git push origin client-build
+
+echo ===== CLIENT BUILD branch updated =====
