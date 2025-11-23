1f761240b25182eea2f3032dd2237bab8863895a
Musaraf Hossain <146651402+musarafhossain@users.noreply.github.com>
2025-11-23 21:49:30 +0530

    Add CORS support and update .gitignore to exclude server and client directories

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import cors from 'cors';
 import { UserRoutes, AuthRoutes } from './routes/index.js';
 
 const app = express();
+
+const corsOptions = {
+  origin: [
+    'https://musaraf.org.in/',
+    'http://localhost:3000'
+  ],
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use('/api/auth', AuthRoutes);
@@ -10,10 +21,10 @@ app.all('/', (req, res) => {
   res.json({ message: 'Server is running' });
 });
 app.use((err, req, res, next) => {
-    res.status(err.status || 500).json({
-        success: false,
-        message: err.message || 'Internal Server Error'
-    });
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message || 'Internal Server Error'
+  });
 });
 
 export default app;
